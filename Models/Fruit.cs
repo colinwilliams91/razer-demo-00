06def8d0f4ef28f3be4fc9c449009349858741fd
colin <colin.williams.dev@gmail.com>
2023-11-29 18:30:38 -0600

    feat: add Fruit Class for EF Model first

---
+++ Models/Fruit.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace razer_demo_00.Models
+{
+    public class Fruit
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        [DataType(DataType.Date)] public DateTime CreatedAt { get; set; }
+    }
+}
