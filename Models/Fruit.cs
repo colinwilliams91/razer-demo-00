b51335ca47fa210e2b827a3703c2b39cf640905e
colin <colin.williams.dev@gmail.com>
2023-11-29 18:33:40 -0600

    chore: comment about DataType decorator and TODO DateTime

--- Models/Fruit.cs
+++ Models/Fruit.cs
@@ -9,5 +9,12 @@ namespace razer_demo_00.Models
         public string? Description { get; set; }
         public decimal Price { get; set; }
         [DataType(DataType.Date)] public DateTime CreatedAt { get; set; }
+        /* 
+         * [DataType(DataType.Date)] Decorator/Attribute specifies the type of data in the property.
+         * With this the user won't be required to enter time information in the date field.
+         * Only the date will be displayed, not the time information.
+         */
+
+        // TODO: Change DataType.Date to DataType.DateTime and see what changes need to occur.
     }
 }
